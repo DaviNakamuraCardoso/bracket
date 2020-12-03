4e9ea6227e4e0aa123452429ca534a8bd8157f97
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2020-12-03 13:35:50 -0300

    Final Project: Remaking the setups and actions

---
+++ clinic/tests.py
@@ -0,0 +1,17 @@
+from django.test import TestCase, Client 
+
+# Create your tests here.
+
+class IndexTestCase(TestCase):
+
+
+    def test_index(self):
+        # Create the client 
+        client = Client()
+
+        # Send a simple get request 
+        response = client.get("")
+
+        # Check the status code 
+        self.assertEqual(response.status_code, 200)
+       
