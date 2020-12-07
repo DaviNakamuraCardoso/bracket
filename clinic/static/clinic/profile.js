4fd0ceab8e7ebe927e32ce5d173660d094d3dd54
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2020-12-06 23:24:25 -0300

    Final Project: Moving the database due to problems with migrations

---
+++ clinic/static/clinic/profile.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // User list info 
+    const info = document.querySelector(".list-info");
+    const edit = document.querySelector("#edit");
+
+    
+
+
+});
