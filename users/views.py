cb07c51a75915a1cf0707eff0b5570f9c79af402
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2020-12-07 20:37:52 -0300

    Final Project: Handling new forms, user interface for registration, new templates

--- users/views.py
+++ users/views.py
@@ -1,25 +1,54 @@
 from django.shortcuts import render, redirect
-from .models import User
+from .models import User, Patient, Clinic, Doctor
 from django.contrib.auth import login, logout, authenticate
 from django.http import HttpResponse 
-from .forms import RegisterForm, LoginForm
+from .forms import RegisterForm, LoginForm, DoctorForm, ClinicForm, PatientForm
 
 # Create your views here.
 def register_view(request): 
     form = RegisterForm() 
+    doctor = DoctorForm()
+    clinic = ClinicForm() 
+    patient = PatientForm()
+
     if request.method == "POST": 
         form = RegisterForm(request.POST)
+        doctor = DoctorForm(request.POST)
+        clinic = ClinicForm(request.POST) 
+        patient = PatientForm(request.POSt)
+
+
         if form.is_valid(): 
             form.save()
             user = User.objects.get(email=form.cleaned_data['email'])
             user.name = get_name(form.cleaned_data['first_name'], form.cleaned_data['last_name'])
 
             user.save()
+
             login(request, user)
+
+            if doctor.is_valid(): 
+                doctor.save()
+                d = Doctor.objects.get(number=doctor.cleaned_data['number'])
+                d.user = user 
+            elif clinic.is_valid(): 
+                clinic.save()
+                c = Clinic.objects.get(name=clinic.cleaned_data['name'])
+                c.manager = user
+            else: 
+                patient.save()
+                p = Patient.objects.get(weight=patient.cleaned_data['weight'])
+                p.user = user 
+
+            
+            
             return redirect('clinic:index') 
     
     return render(request, 'users/register.html', {
-        'form': form
+        'form': form, 
+        'doctor': doctor, 
+        'patient': patient, 
+        'clinic': clinic 
     })
 
 
