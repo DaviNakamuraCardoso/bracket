ecf3a4454934303f98e85b6bf666465bfe07a97a
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2020-12-09 22:56:22 -0300

    Final Project: Adding different forms for each user with ModelForms

--- users/forms.py
+++ users/forms.py
@@ -1,30 +1,31 @@
 from django.contrib.auth.forms import UserCreationForm 
 from .models import User, Patient, Doctor, Clinic  
 from django import forms
+from django.forms import ModelForm
 
 
 class RegisterForm(UserCreationForm): 
     class Meta: 
         model = User 
-        fields = ['email', 'first_name', 'last_name', 'password1', 'password2']
+        fields = ['email', 'first_name', 'last_name', 'user_type', 'password1', 'password2']
 
 class LoginForm(forms.Form): 
     username = forms.CharField(max_length=64)
     password = forms.CharField(max_length=64, widget=forms.PasswordInput())
 
 
-class PatientForm(forms.Form): 
+class PatientForm(ModelForm): 
     class Meta: 
         model = Patient 
-        fields = ['weight', 'height'] 
+        fields = ['weight', 'height']
     
 
-class DoctorForm(forms.Form): 
+class DoctorForm(ModelForm): 
     class Meta: 
         model = Doctor 
         fields = ['number', 'degree']
     
-class ClinicForm(forms.Form): 
+class ClinicForm(ModelForm): 
     class Meta: 
         model = Clinic 
         fields = ['name', 'email', 'city']
