7253b971e5e9b44847a7fd95248c9bc24490b21d
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2020-12-11 14:47:51 -0300

    Final Project: Creating a SQL database for testing

--- bracket/settings.py
+++ bracket/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
 if 'test' in sys.argv: 
     DATABASES['default'] = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', 
-        'NAME': 'bracket0', 
+        'NAME': 'test.sql', 
         'HOST': '127.0.0.1', 
         'PASSWORD': 'davi123', 
         'USER': 'davi', 
