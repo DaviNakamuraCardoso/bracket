e51fe03f7495855ee7410ef8173d7b5829a79078
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2020-12-17 12:37:18 -0300

    Final Project: Refactoring the JavaScript code for the registration

--- users/static/users/register.js
+++ users/static/users/register.js
@@ -5,7 +5,6 @@ document.addEventListener('DOMContentLoaded', () =>
     if (title.innerHTML == 'Patient')
     {
         registerPatient();
-        console.log("patient it is");
     }
     else if (title.innerHTML == 'Doctor')
     {
@@ -16,113 +15,81 @@ document.addEventListener('DOMContentLoaded', () =>
 });
 
 
-
 function listChoices(array, field, list)
 {
      
-        // Adding the allergy value to the list 
-        array.push(field.value);
+    // Adding the allergy value to the list 
+    array.push(field.value);
 
-        // Creating an li element to show the allergy
-        const li = document.createElement('li');
-        const btn = document.createElement('button');
-        const val = document.createElement('span');
+    // Creating an li element to show the allergy
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
+    const val = document.createElement('span');
 
-        val.innerHTML = field.value;
-        btn.innerHTML = 'x';
+    val.innerHTML = field.value;
+    btn.innerHTML = 'x';
 
-        btn.addEventListener('click', () => {
+    btn.addEventListener('click', () => {
 
-            // Deletes the list element when clicked 
-            const element = btn.parentElement;
-            const v = element.firstChild;
-            const index = array.indexOf(v.innerHTML);
+        // Deletes the list element when clicked 
+        const element = btn.parentElement;
+        const v = element.firstChild;
+        const index = array.indexOf(v.innerHTML);
             
-            array.splice(index, 1);
-            element.remove();
-        });
+        array.splice(index, 1);
+        element.remove();
+    });
 
+    // Adding the allergy to the list
+    li.append(val);
+    li.append(btn);
+    list.append(li);
 
-        // Adding the allergy to the list
-        li.append(val);
-        li.append(btn);
-        list.append(li);
-        // Reset the textarea value
-        field.value = '';
-
-
-    
+    // Reset the field value
+    field.value = '';
 }
+
 
 function registerPatient()
 {
-    // Allergies
-    let allergiesArray = [];
-    const allergiesInput = document.querySelector("#id_temp_allergies");
-    const allergies = document.querySelector("#allergies");
-    const allergiesList = document.querySelector("#id_temp_allergies_ul");
-    
-    allergiesInput.onchange = function() 
-    {
-        listChoices(allergiesArray, allergiesInput, allergiesList);
-    }
 
-    // Conditions
-    
-    let conditionsArray = [];
-    const conditions = document.querySelector("#conditions");
-    const conditionsInput = document.querySelector("#id_temp_conditions");
-    const conditionsList = document.querySelector("#id_temp_conditions_ul");
-     
-    conditionsInput.onchange = function()
-    {
-        listChoices(conditionsArray, conditionsInput, conditionsList);
-    }
+    // Allergies 
+    watchSelection('allergies');
 
-    // Medications
-    let medicationsArray = [];
-    const medicationsInput = document.querySelector("#id_temp_medications");
-    const medications = document.querySelector("#medications");
-    const medicationsList = document.querySelector("#id_temp_medications_ul");
+    // Medications 
+    watchSelection('medications');
 
-    medicationsInput.onchange = function()
-    {
-        listChoices(medicationsArray, medicationsInput, medicationsList);
-    }
-    
-    
-    const form = document.querySelector('form');
-    form.onsubmit = () => 
-    {
-        allergies.value = allergiesArray.join();
-        conditions.value = conditionsArray.join();
-        medications.value = medicationsArray.join();
-        
-        return true;
-    }
+    // Conditions 
+    watchSelection('conditions');
+
 }
 
 
 function registerDoctor()
 {
-    const areas = document.querySelector("#areas");
-    let areasArray = [];
-    const areasList = document.querySelector("#list");
-    const areasInput = document.querySelector("#id_temp_areas");
-    areas.style.display = 'none';
-
-    areasInput.onchange = (event) => 
-    {
-        listChoices(event, areasArray, areasInput, areasList);
-
-    }
-
-    const form = document.querySelector("form");
-    form.onsubmit = () => 
-    {
-        areas.value = areasArray.join();
-        return true;
-    }
-    
-
+    // Areas 
+    watchSelection("areas");
 }
+
+function watchSelection(id)
+{
+    // Getting the essential elements 
+    let array = [];
+    const element = document.querySelector(`#${id}`);
+    const elementInput = document.querySelector(`#id_temp_${id}`);
+    const elementList = document.querySelector(`#id_temp_${id}_ul`);
+    const form = document.querySelector('form');
+
+    elementInput.onchange = function() 
+    {
+        listChoices(array, elementInput, elementList);
+    }
+
+    form.addEventListener("submit", () => 
+    {
+        element.value = array.join();
+    });
+}
+
+        
+    
