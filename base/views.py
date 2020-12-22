6c9b8b13364806d35792e7249b1810a04d1898be
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2020-12-21 21:18:33 -0300

    Final Project: Adding forms to get the user location

--- base/views.py
+++ base/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect 
 from clinics.models import Clinic
-
-
+from clinics.utils import get_clinic 
 # Create your views here.
 
 def index(request): 
-    if request.user.is_clinic: 
-        clinic = Clinic.objects.get(user__name=request.user.name)
         
     return render(request, 'base/index.html', {
         'user': request.user, 
-        'clinic': clinic 
+        'clinic': get_clinic(request) 
     })
