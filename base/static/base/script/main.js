abda2f5e1fefdd321f1baff3389b1c3d98889c14
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2021-01-16 17:30:23 -0300

    Final Project: Bug fixes

--- base/static/base/script/main.js
+++ base/static/base/script/main.js
@@ -3,27 +3,9 @@ document.addEventListener("DOMContentLoaded", () =>
     const icons = document.querySelectorAll(".icon");
 
 
-    icons.forEach(icon => {
+    icons.forEach(icon => addIconListeners(icon));
     
-        const box = icon.parentElement.lastElementChild;
-        icon.addEventListener('click', () => {
-            box.classList.toggle('open');
-            
-        });
-        box.onmouseover = () => {
-            document.removeEventListener('click');
-        
-        }
-        box.onmouseout = () => {
-            document.addEventListener('click', () => {
-                box.classList.remove('open');
-            });
-        }
-
-
-
-    });
-        const notificationsLength = document.querySelectorAll('.notifi-item').length;
+    const notificationsLength = document.querySelectorAll('.notifi-item').length;
 
     if (notificationsLength > 0)
     {
@@ -35,4 +17,24 @@ document.addEventListener("DOMContentLoaded", () =>
 });
 
 
+function toggleClass(box) 
+{
+    box.classList.toggle('open');
+}
+
+
+function removeClass(box)
+{
+    box.classList.remove('open');
+}
+
+function addIconListeners(icon)
+{
+    const box = icon.parentElement.lastElementChild;
+
+    icon.onclick = () => {
+        toggleClass(box);
+    }
+
+}
         
