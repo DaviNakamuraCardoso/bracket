9ded421e1acd921a7cf1561bbc601fc9e040de76
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2021-01-16 17:50:28 -0300

    Final Project: Adding a register form for unregistered users

--- base/views.py
+++ base/views.py
@@ -2,14 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect 
 from clinics.models import Clinic
 from clinics.utils import get_clinic 
+from users.forms import RegisterForm
 # Create your views here.
 
 def index(request): 
     """Render all the clinics in the same city as the user."""
+    form = RegisterForm()
+
+    if request.method == "POST": 
+        form = RegisterForm(request.POST)
         
     return render(request, 'base/index.html', {
         'user': request.user, 
-        'clinics': Clinic.objects.all()
+        'clinics': Clinic.objects.all(), 
+        'form': form
     })
 
 
