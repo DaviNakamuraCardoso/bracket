58eab3597255ea84aeb21e1d9580362f703929e2
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2021-01-16 21:18:22 -0300

    Final Project: Changes in the form structure to allow style modifications

--- users/forms.py
+++ users/forms.py
@@ -17,6 +17,12 @@ class DateInput(forms.DateInput):
 
 
 class RegisterForm(UserCreationForm): 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
+
+        
     class Meta: 
         model = User 
         fields = ['email', 'first_name', 'last_name', 'user_type', 'password1', 'password2']
