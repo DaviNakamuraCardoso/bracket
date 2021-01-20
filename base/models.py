571b5c2c598f2f96e130c8c33e5b94f656ec870b
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2021-01-20 19:20:17 -0300

    Final Project: Changing notification model to support async confirmation

--- base/models.py
+++ base/models.py
@@ -5,10 +5,11 @@ from base.time import strfdelta
 # Create your models here.
 class Notification(models.Model): 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
-    origin = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_notifications', blank=True, null=True)
+    origin = models.CharField(max_length=64, blank=True, null=True)
     text = models.CharField(max_length=128)
     timestamp = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     read = models.BooleanField(default=False)
+    url = models.URLField(max_length=64, blank=True, null=True)
 
 
     def __str__(self): 
