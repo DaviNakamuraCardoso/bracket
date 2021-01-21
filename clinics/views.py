cb8549f432cc43ad83b4182368386c45886d26af
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2021-01-21 16:53:51 -0300

    Final Project: Backend for handling async requests

--- clinics/views.py
+++ clinics/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from clinics.models import Clinic 
 import json 
 
@@ -20,11 +21,26 @@ def profile(request, clinic_name):
         'data': clinic.serialize()
     })
 
+
+@csrf_exempt 
 def invitation(request, clinic_name): 
     clinic = Clinic.objects.get(clinic_name=clinic_name)
+    doctor = request.user.doctor
     if request.method == "PUT": 
         data = json.loads(request.body)
+        if data['confirm']: 
+            clinic.doctors.add(doctor)
+            return JsonResponse({"message": f"Succesfully joined {clinic.name}"}, status=204)
+        else: 
+            request.user.notifications.get(origin=clinic.name).delete()
+            return JsonResponse({"message": f"Succesfully refused to join {clinic.name}"}, status=204) 
+    
+    return JsonResponse({"message": "Method must be PUT."}, status=400)
+            
+
+
+    
+
         
-    return HttpResponse('OK')
 
     
