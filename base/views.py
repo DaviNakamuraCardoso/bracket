71a02435ca638d2207645096a4937f95d0f67a3b
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2021-01-28 21:23:38 -0300

    Final Project: Adding the doctor form to the index page

--- base/views.py
+++ base/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect 
 from clinics.models import Clinic
-from users.forms import PatientForm, BaseForm
+from users.forms import PatientForm, BaseForm, DoctorForm
 # Create your views here.
 
 def index(request): 
     """Render all the clinics in the same city as the user."""
     form = PatientForm()
     base_form = BaseForm()
+    doctor_form = DoctorForm()
         
     return render(request, 'base/index.html', {
         'user': request.user, 
         'clinics': Clinic.objects.all(), 
         'form': form, 
-        'base_form': base_form 
+        'base_form': base_form, 
+        'doctor_form': doctor_form
     })
 
 
