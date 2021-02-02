23727acabcb1fef7121d9e2da9e8283c0d941631
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2021-02-02 19:41:55 -0300

    Final Project: User Inteface for creating shifts

--- doctors/static/doctors/scripts/schedule.js
+++ doctors/static/doctors/scripts/schedule.js
@@ -2,27 +2,64 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const checkBoxes = document.querySelectorAll(".day-input");
     const nextButton = document.querySelector("#next"); 
+    const formContainer = document.querySelector("#form-container"); 
+    const container = document.querySelector('#container'); 
+    nextButton.onclick = () => {
+
+        let index = 0;
         checkBoxes.forEach(checkBox => {
             if (checkBox.checked)
             {
-                console.log(checkBox.value);                
+                const day = checkBox.parentElement.querySelector('.day-span').innerHTML; 
+                const mainDiv = document.createElement('div');
+                const button = document.createElement("button"); 
+                const cp = copyElement(formContainer, day, index); 
+                const dayContainer = document.createElement('div'); 
+                const input = document.createElement('input'); 
+
+                mainDiv.id = day; 
+                input.type = "hidden"; 
+                input.value = index+1;
+
+                button.type = 'button'; 
+                button.innerHTML = `New Shift for ${day}`; 
+                button.onclick = () => {
+                    index++; 
+                    input.value = index+1;
+                    
+                    dayContainer.append(copyElement(formContainer, day, index)); 
+                }
+
+                dayContainer.append(cp);
+                
+                mainDiv.append(dayContainer); 
+                mainDiv.append(input); 
+                mainDiv.append(button); 
+                container.append(mainDiv); 
             }
         });
-        
-    updateTimePickers(); 
-
+    }
+    
 });
 
-
-function updateTimePickers() 
+function copyElement(element, tag, index)
 {
-    const pickers = document.querySelectorAll('.picker'); 
-    pickers.forEach(picker => {
-        picker.addEventListener('change', () => {
+    const newElement = element.cloneNode(true);
+    const descendents = newElement.getElementsByTagName("*");
+    for (var i = 0; i < descendents.length; i++)
+    {
+        var e = descendents[i];
+        if (e.id)
+        {
+            e.id = `${e.id}_${tag}_${index}`;
+            e.name = `${e.name}_${tag}_${index}`
 
-            let date = picker.value; 
-            picker.parentElement.querySelector(".time").value = date;
-        });
+        }
+    }
+    
+    newElement.id = `${tag}_${index}`;
 
-    }); 
+    return (newElement);
+
 }
+
