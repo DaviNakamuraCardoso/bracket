2c83a98844b465a8034438f858cdf6f61076f436
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2021-02-02 21:55:47 -0300

    Final Project: Styles for the shift forms

--- doctors/static/doctors/scripts/schedule.js
+++ doctors/static/doctors/scripts/schedule.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    const checkBoxes = document.querySelectorAll(".day-input");
+    const checkBoxes = document.querySelectorAll(".checkbox-input");
     const nextButton = document.querySelector("#next"); 
     const formContainer = document.querySelector("#form-container"); 
     const container = document.querySelector('#container'); 
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
         checkBoxes.forEach(checkBox => {
             if (checkBox.checked)
             {
-                const day = checkBox.parentElement.querySelector('.day-span').innerHTML; 
+                const day = checkBox.parentElement.querySelector('.checkbox-span').innerHTML; 
                 const mainDiv = document.createElement('div');
                 const button = document.createElement("button"); 
                 const cp = copyElement(formContainer, day, index); 
@@ -21,6 +21,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 input.type = "hidden"; 
                 input.value = index+1;
 
+                dayContainer.className = "day-container";
+
                 button.type = 'button'; 
                 button.innerHTML = `New Shift for ${day}`; 
                 button.onclick = () => {
@@ -30,6 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     dayContainer.append(copyElement(formContainer, day, index)); 
                 }
 
+                
                 dayContainer.append(cp);
                 
                 mainDiv.append(dayContainer); 
@@ -56,8 +59,10 @@ function copyElement(element, tag, index)
 
         }
     }
+    newElement.querySelector('.day-title').innerHTML = `${tag} 0${index+1}`;
     
     newElement.id = `${tag}_${index}`;
+    newElement.className = "day-card";
 
     return (newElement);
 
