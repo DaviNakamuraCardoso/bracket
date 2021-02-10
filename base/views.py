dac863bf46c904d34c1928ddf1d32de3626cb019
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2021-02-09 23:18:52 -0300

    Final Project: Adding a search bar to the menu

--- base/views.py
+++ base/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect 
 from clinics.models import Clinic
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from users.forms import FORMS_CONTEXT
 # Create your views here.
 
 def index(request): 
     """Render all the clinics in the same city as the user."""
-    local_context = {'user': request.user, 'clinics': Clinic.objects.all()}
+
+    clinics = Clinic.objects.all()[:10]
+    if search := request.GET.get('query'): 
+        vector = SearchVector('name')
+        query = SearchQuery(search)
+
+
+        clinics = Clinic.objects.annotate(
+            rank=SearchRank(vector, query)
+        ).order_by('-rank')
+
+    
+    local_context = {'user': request.user, 'clinics': clinics}
     context = {**local_context, **FORMS_CONTEXT}
+        
         
     return render(request, 'base/index.html', context=context)
 
