a388bfddf3c4eb5b75aa09db1139e335beb9e699
DaviNakamuraCardoso <you@example.com>
2021-02-11 22:40:06 -0300

    Final Project: Settings to support image uploads

--- bracket/urls.py
+++ bracket/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include 
+from django.conf import settings 
+from django.conf.urls.static import static 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,5 +24,8 @@ urlpatterns = [
     path('', include('base.urls')), 
     path('patients/', include('patients.urls')), 
     path('doctors/', include('doctors.urls')), 
-    path('clinics/', include('clinics.urls'))
+    path('clinics/', include('clinics.urls')), 
+
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
