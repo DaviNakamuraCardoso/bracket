d52f7f2e57bbe72cbf202f7dbeaa0e6bb533e9b0
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2021-02-17 21:43:35 -0300

    Final Project: Creating the User Interface to async invite/accept/deny/add doctors to clinics

--- clinics/static/clinics/scripts/invite.js
+++ clinics/static/clinics/scripts/invite.js
@@ -1,45 +1,113 @@
 
-
-function inviteHandler(button)
+/**
+* @name main
+* @function
+* @global
+*/
+function main()
 {
-    button.style.pointerEvents = "none";
-    let value = button.dataset.val;
-    const isInvite = ((value == "accept") ? true : false);
-    
-    fetch(button.dataset.url, {
-        method: "PUT", 
-        body: JSON.stringify({
-            invite: isInvite 
-        })
-    })
-    .then(response => response.json())
-    .then(result => {
-        console.log(result.message);
-        if (value == "accept")
-        {
-            button.dataset.val = "deny";
-            button.innerHTML = "Cancel Invitation";
+    // Get the invite button
+    const button = document.querySelector("#invite");
+
+    // If it is a clinic admin adding a doctor, call the handleAdd function
+    if (button.dataset.type == "add")
+    {
+        button.onclick = () => {
+            handleAdd(this);
         }
-        else 
-        {
-            button.dataset.val = "accept"; 
-            button.innerHTML = "Invite";
+    }
+    // If it is a doctor asking to join a clinic, call the handleAsk function
+    else if (button.dataset.type == "ask")
+    {
+        button.onclick = () => {
+            handleAsk(this);
         }
-        
-    });
-
-    button.style.pointerEvents = "auto";
-
-	
-
+    }
 }
 
 
-document.addEventListener("DOMContentLoaded", () => {
-	const button = document.getElementById('invite'); 
-    handleInvite(button); 
+/**
+* @name handleAdd
+* @function
+* @global
+* @param {Element} button - The button that was clicked
+*/
+function handleAdd(button)
+{
+    // Prevents the user from double clicking the button
+    button.style.pointerEvents = 'none';
 
-	
-	
-	
-});
+    // Send a get request to the server, asking for the right set of clinics
+    fetch(button.dataset.url)
+
+    // Parse the response to JSON
+    .then(response => response.json())
+
+    // The server return a array of objects with clinic id and clinic name
+    .then(clinics => {
+
+        // Selects the popUp datalist
+        const popUp = document.querySelector("#pop-up");
+
+        // Adds the clinics to the datalist
+        for (let i = 0; i < clinics.clinics.length; i++)
+        {
+            let option = document.createElement('option');
+            option.value = clinics.clinics[i].id;
+            option.label = clinics.clinics[i].name;
+            popUp.append(option);
+        }
+
+        // Show the form
+        const form = popUp.parentElement;
+        form.className = 'show';
+
+    })
+}
+
+
+/**
+* @name handleAsk
+* @function
+* @global
+* @param {Element} button - The clicked button
+*/
+function handleAsk(button)
+{
+
+    // Prevents double click
+    button.style.pointerEvents = 'none';
+
+    // CSRF token to prevent Cross-origin attacks
+    const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+    const request = new Request(
+        button.dataset.url,
+        {headers: {'X-CSRFToken': csrftoken}}
+
+    );
+
+    // Invite variable is true when this is a request to join the clinic, and
+    // false when is the user wants to cancel the invitation
+    const invite = (button.dataset.value == 'request') ? true : false;
+
+    // Fetches the url with the CSRF token
+    fetch(request, {
+        method: 'POST',
+        mode: 'same-origin',
+        body: JSON.stringify({
+            'invite': invite
+        })
+    })
+
+    // Parse the response to JSON
+    .then(response => response.json())
+
+    // Display the message as a notification
+    .then(result => {
+        console.log(result.message);
+    });
+
+
+}
+
+document.addEventListener("DOMContentLoaded", main);
