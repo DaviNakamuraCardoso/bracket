0bf74ddbf43084f5c694f54bb3276341839f30c4
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2021-03-02 22:44:44 -0300

    Final Project: Creating the rate model

--- base/models.py
+++ base/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from users.models import User
 from clinics.models import Clinic
+from doctors.models import Doctor
 from base.time import strfdelta
 
 # Create your models here.
@@ -16,6 +17,22 @@ class Notification(models.Model):
 
     def __str__(self):
         return f"{self.user.name}: {self.text} on {self.timestamp}"
+
+    def time(self):
+        return strfdelta(timestamp=self.timestamp)
+
+class Rate(models.Model):
+    clinic = models.ForeignKey(Clinic, on_delete=models.CASCADE, related_name="ratings", blank=True, null=True)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, related_name="ratings", blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="ratings", blank=True, null=True)
+
+    rate = models.FloatField()
+    is_doctor_rating = models.BooleanField(default=False)
+    comment = models.CharField(max_length=280)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.__str__()}'s rate"
 
     def time(self):
         return strfdelta(timestamp=self.timestamp)
