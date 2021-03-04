862c880f6629d2bc42be89a26e7293755c91e89b
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2021-03-03 21:09:47 -0300

    Final Project: Template changes to implement Oauth

--- doctors/urls.py
+++ doctors/urls.py
@@ -9,13 +9,12 @@ urlpatterns = [
 
     path("<str:name>", views.profile, name="profile"),
     path("<str:name>/about", views.profile, name="about"),
-    path("<str:name>/ratings", views.profile, name="ratings"), 
+    path("<str:name>/ratings", views.profile, name="ratings"),
     path("<str:name>/add", views.add, name="add"),
     path("<str:name>/accept", views.accept, name="accept"),
     path("<str:name>/schedule", views.schedule_view, name="schedule"),
     path("<str:name>/days/<str:clinic>/<str:area>", views.schedule_days, name="days"),
     path("<str:name>/appointment", views.new_appointment, name="new-appointment"),
-
 
 
     path("<str:name>/<int:year>/<int:month>/<int:day>", views.day_planner, name="day-planner"),
